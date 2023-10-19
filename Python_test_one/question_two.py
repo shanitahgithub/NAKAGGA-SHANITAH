a8c57ff0136a8dfc2ab18469c790dba1c9416d16
shanitahgithub <142230234+shanitahgithub@users.noreply.github.com>
2023-10-19 22:13:12 +1400

    Python_Test_One

--- Python_test_one/question_two.py
+++ Python_test_one/question_two.py
@@ -1,3 +1,4 @@
+
 def average_of_two_numbers(x,y):
     output=(x+y)/2
     print(output)
@@ -8,11 +9,12 @@ num1=int(input("Enter the num1:"))
 num2=int(input("Enter the num2:"))
 num3=int(input("Enter the num3:"))
 if num1<num2 and num2<num3:
-        print(f"{'num1'} is less than {'num2'} and {'num3'}")
+        print(f"{num1} is less than {num2} and {num3}")
 elif num2<num1 and num2<num3:
-     print(f"{'num2'} is less than {'num1'} and {'num3'}")
+     print(f"{num2} is less than {num1} and {num3}")
 else:
-       print(f"{'num3'} is less than {'num1'} and {'num2'}")
+       print(f"{num3} is less than {num1} and {num2}")
+
 
     
 
